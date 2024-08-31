7fca99da735254093e390b7c0f26d348d6365034
ailtonruby <ailtonruby97@gmail.com>
2024-08-30 22:25:32 -0300

    exercicio

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        numero num = new numero();
+        num.setNum(2);
+        System.out.println(num.getNum());
+    }
+}
---
+++ src/numero.java
@@ -0,0 +1,11 @@
+public class numero {
+    Integer num;
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num= num;
+    }
+}
